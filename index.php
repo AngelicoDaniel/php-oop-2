d2410020c0e4ddc1c4ee0aa90b49aea66aa2f3ed
Daniel <angelicodaniel93@gmail.com>
2022-12-19 20:44:49 +0100

    creato array

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php 
 
+include_once __DIR__ . './category.php';
+include_once __DIR__ . './cibo.php';
+include_once __DIR__ . './accessori.php';
+include_once __DIR__ . './giocattoli.php';
+
+$arrCategory = [
+    'cane' => new Category ('cane', 'icona_cane'),
+    'uccello' => new Category ('uccello', 'icona_uccello'),
+    'gatto' => new Category ('gatto', 'icona_gatto')
+
+];
 
 
 ?>
